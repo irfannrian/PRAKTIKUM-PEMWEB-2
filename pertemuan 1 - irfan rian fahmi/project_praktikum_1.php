14717fdc7a9b4358b41e1d91237244bf58fd9ab7
irfannrian <93029077+irfannrian@users.noreply.github.com>
2022-02-26 15:23:42 +0700

    Add files via upload

---
+++ pertemuan 1 - irfan rian fahmi/project_praktikum_1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles/css/style.css" />
+  <title>Document</title>
+</head>
+<body>
+    
+  <h1>array_buah.php</h1>
+
+  <div id="container">
+    <article class="holder">
+      <section class="card">
+        <div class="card__text">
+          <h2>Pepaya</h2>
+        </div>
+      </section>
+    </article>
+    <article class="holder">
+      <section class="card">
+        <div class="card__text">
+          <h2>Mangga</h2>
+        </div>
+      </section>
+    </article>
+    <article class="holder">
+      <section class="card">
+        <div class="card__text">
+          <h2>Pisang</h2>
+        </div>
+      </section>
+    </article>
+    <article class="holder">
+      <section class="card">
+        <div class="card__text">
+          <h2>Jambu</h2>
+        </div>
+      </section>
+    </article>
+  </div>
+
+</body>
+</html>
