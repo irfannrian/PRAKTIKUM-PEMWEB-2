c5638e434dd0d1c39c0fb3e39a8aa6835b7cdbf8
irfannrian <93029077+irfannrian@users.noreply.github.com>
2022-03-18 15:52:13 +0700

    Add files via upload

---
+++ praktikum 3 - irfan rian fahmi/startbootstrap-sb-admin-gh-pages/view/home.php
@@ -0,0 +1,9 @@
+<?php
+include_once '../layout/header.php'
+?>
+
+<h1>Tugas Praktikum Pemweb 3</h1>
+
+<?php
+include_once'../layout/footer.php'
+?>
---
+++ praktikum 3 - irfan rian fahmi/web dinamis/artikel1.php
@@ -0,0 +1,9 @@
+<?php
+include_once 'header.php'
+?>
+
+<h1>Praktikum</h1>
+
+<?php
+include_once 'footer.php'
+?>
